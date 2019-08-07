bc88ed50e623ccc46c8906cff505b93bcaf04b36
Bannette <banetaloo3@gmail.com>
2019-08-07 13:51:08 +0300

    create class endenger

--- src/main/java/models/Endanger.java
+++ src/main/java/models/Endanger.java
@@ -1,18 +1,24 @@
 package models;
 
 
-public class Endangered {
+import org.sql2o.Connection;
+
+
+public class Endanger {
     private String animal_name;
     private String animal_age;
     private String animal_health;
     private String location;
+    private int animal_id;
 
-    public Endangered(String animal_name,String animal_age,String  animal_health ,String location) {
+
+    public Endanger(String animal_name, String animal_age, String animal_health, String location) {
         this.animal_name = animal_name;
         this.animal_age = animal_age;
         this.animal_health = animal_health;
         this.location = location;
     }
+
 
     public String getAnimal_name() {
         return animal_name;
@@ -29,6 +35,19 @@ public class Endangered {
     public String getLocation() {
         return location;
     }
-}
 
+
+
+
+    public void saveSightedAnimal(Sighting sighting) {
+        try (Connection conn = Database.sql2otest.open()) {
+            String sql = "INSERT INTO  sightings(animal_id, animal_location, ranger_name ) VALUES (:animal_id, :animal_location, :ranger_name);";
+            this.animal_id = (int) conn.createQuery(sql, true)
+                    .addParameter("animal_location", this.location)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+}
 
