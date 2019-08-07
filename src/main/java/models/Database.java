b933224ee3eb81c6bd1cae1e64311920ab26d2a7
Bannette <banetaloo3@gmail.com>
2019-08-07 13:48:36 +0300

    create database models

--- src/main/java/models/Database.java
+++ src/main/java/models/Database.java
@@ -1,6 +1,11 @@
 package models;
 
-public class Database {
-}
 
+import org.sql2o.Sql2o;
+
+public class Database {
+    public static Sql2o sql2otest = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "user", "666Nikol-");
+
+
+}
 
