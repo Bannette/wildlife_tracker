7529d475481385d85d5088b854b005aeb50499e9
Bannette <banetaloo3@gmail.com>
2019-08-07 16:53:35 +0300

    modified database

--- src/main/java/models/Database.java
+++ src/main/java/models/Database.java
@@ -4,7 +4,7 @@ package models;
 import org.sql2o.Sql2o;
 
 public class Database {
-    public static Sql2o sql2otest = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "user", "666Nikol-");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "user", "666Nikol-");
 
 
 }
