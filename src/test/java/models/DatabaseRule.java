2cd9211a89ed247f8debc57367099d1ac2364423
Bannette <banetaloo3@gmail.com>
2019-08-07 14:07:23 +0300

    create model rule

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -1,6 +1,30 @@
 package models;
 
-public class DatabaseRule {
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+
+
+
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        Database.sql2otest = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "user","666Nikol-");
+    }
+    @Override
+    protected void after(){
+        try(Connection con=Database.sql2otest.open()){
+            String deleteAnimalsQuery ="DELETE FROM animals *;";
+            String deleteSightingsQuery="DELETE FROM sightings;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+//            con.createQuery(deleteSightingsQuery).executeUpdate();
+        }
+    }
+
+
 }
+
 
 
