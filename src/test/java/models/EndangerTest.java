4f5c8336e8a9eb8027abe368f08a87d56d1f37b9
Bannette <banetaloo3@gmail.com>
2019-08-07 14:08:24 +0300

    run endengered test

--- src/test/java/models/EndangerTest.java
+++ src/test/java/models/EndangerTest.java
@@ -16,19 +16,36 @@ public class EndangerTest {
     public void tearDown() throws Exception {
     }
 
+    public Endanger setUpNewEndanger() {
+        return new Endanger("Lion", "20", "weak", "River");
+    }
+
     @Test
     public void getAnimal_name() {
+        Endanger newEndanger = setUpNewEndanger();
+        assertEquals("Lion", newEndanger.getAnimal_name());
     }
 
     @Test
     public void getAnimal_age() {
+        Endanger newEndanger = setUpNewEndanger();
+        newEndanger.getAnimal_age();
+        assertEquals("20", newEndanger.getAnimal_age());
+
     }
 
     @Test
     public void getAnimal_health() {
+        Endanger newEndanger = setUpNewEndanger();
+        newEndanger.getAnimal_health();
+        assertEquals("weak", newEndanger.getAnimal_health());
     }
 
     @Test
     public void getLocation() {
+        Endanger newEndanger = setUpNewEndanger();
+        newEndanger.getLocation();
+        assertEquals("River", newEndanger.getLocation());
     }
 }
+
